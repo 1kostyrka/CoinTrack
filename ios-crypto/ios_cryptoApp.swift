0705d6de75a766b9a9591fadb1e9969a7db99dae
Ivan Kostyrka <boss.kostarika@gmail.com>
2024-12-23 13:45:21 +0200

    Initial Commit

---
+++ ios-crypto/ios_cryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_cryptoApp.swift
+//  ios-crypto
+//
+//  Created by Ivan Kostyrka on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ios_cryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
